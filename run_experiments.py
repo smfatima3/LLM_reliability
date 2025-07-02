2dc4a34d1b03aedf24325588879946f0451d44c7
smfatima3 <s.m.fatima13@gmail.com>
2025-07-02 15:17:26 +0500

    Update run_experiments.py

--- run_experiments.py
+++ run_experiments.py
@@ -1,7 +1,7 @@
 import itertools
 import copy
 import os
-from validation_simulator import TrainingSimulator, CONFIG as BASE_CONFIG
+from simulator import TrainingSimulator, CONFIG as BASE_CONFIG
 
 # --- Step 1: Define the Experiment Matrix ---
 
