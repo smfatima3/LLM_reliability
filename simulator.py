7a92d198702778d3f88c2c049b955fbe80e7184b
smfatima3 <s.m.fatima13@gmail.com>
2025-07-02 18:28:12 +0500

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -21,7 +21,7 @@ CONFIG = {
     'r_metric_weights': {
         'w1_lambda': 0.1,   # Proven to be unhelpful, so it's removed.
         'w2_sigma_sq': 0.5, # Given higher importance due to better precision.
-        'w3_delta_l': 0.65,  # Kept for its high recall, but with reduced influence.
+        'w3_delta_l': 0.85,  # Kept for its high recall, but with reduced influence.
     }
 }
 
