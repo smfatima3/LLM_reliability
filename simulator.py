67eb0edf32d2ff3b22e9d93b03a3dee9881b1b29
smfatima3 <s.m.fatima13@gmail.com>
2025-07-03 08:26:24 +0500

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -20,8 +20,8 @@ CONFIG = {
     # Based on the ablation study, we re-balance the weights to lower the False Positive Rate.
     'r_metric_weights': {
         'w1_lambda': 0.0,   # Proven to be unhelpful, so it's removed.
-        'w2_sigma_sq': 0.51, # Given higher importance due to better precision.
-        'w3_delta_l': 0.76,  # Kept for its high recall, but with reduced influence.
+        'w2_sigma_sq': 0.5, # Given higher importance due to better precision.
+        'w3_delta_l': 0.78,  # Kept for its high recall, but with reduced influence.
     }
 }
 
